78e71f0b48396fe7bb16a0dc8d28f731661b65c7
porosh2002 <jamilkashem2002@gmail.com>
2021-03-26 02:02:31 +0600

    first commit

---
+++ App.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import PyPDF2
+# PDF File
+Book = open('give-and-take.pdf','rb')
+# PDF Reader
+PdfReader = PyPDF2.PdfFileReader(Book)
+# Text to Speech
+TextToSpeech = pyttsx3.init()
+# Get Specific page
+page = PdfReader.getPage(31)
+# Page Text
+text = page.extractText()
+# Speech Text
+TextToSpeech.say(text)
+TextToSpeech.runAndWait()
