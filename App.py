daeb33c1295c13713cf298040a357407a008988b
porosh2002 <jamilkashem2002@gmail.com>
2021-03-26 02:07:23 +0600

    PDF file path updated

--- App.py
+++ App.py
@@ -1,13 +1,13 @@
 import pyttsx3
 import PyPDF2
 # PDF File
-Book = open('SamplePDF','rb')
+Book = open('SamplePDF.pdf','rb')
 # PDF Reader
 PdfReader = PyPDF2.PdfFileReader(Book)
 # Text to Speech
 TextToSpeech = pyttsx3.init()
 # Get Specific page
-page = PdfReader.getPage(31)
+page = PdfReader.getPage(1)
 # Page Text
 text = page.extractText()
 # Speech Text
