17c3480b4f8b31bc2e8894b61080e773d8c3c759
porosh2002 <jamilkashem2002@gmail.com>
2021-03-29 01:23:32 +0600

    2.0.0

--- App.py
+++ App.py
@@ -1,15 +1,41 @@
+# Dependency
 import pyttsx3
 import PyPDF2
+from gtts import gTTS
+
+
 # PDF File
 Book = open('SamplePDF.pdf','rb')
+
+
 # PDF Reader
 PdfReader = PyPDF2.PdfFileReader(Book)
-# Text to Speech
-TextToSpeech = pyttsx3.init()
-# Get Specific page
-page = PdfReader.getPage(1)
-# Page Text
+
+
+# Pages 
+# PagesNumber = PdfReader.numPages
+# print(PagesNumber)
+
+
+# Text to Speech Engine
+TextToSpeechEngine = pyttsx3.init()
+
+
+# Get Specific Page
+page = PdfReader.getPage(0)
+
+
+# Specific Page Text
 text = page.extractText()
+
+
 # Speech Text
-TextToSpeech.say(text)
-TextToSpeech.runAndWait()
+# TextToSpeechEngine.say(text)
+# TextToSpeechEngine.runAndWait()
+
+
+# Save As Audio 
+tts = gTTS(text=text, lang='en')
+tts.save("saved_file.mp3")
+print("File saved!")
+
